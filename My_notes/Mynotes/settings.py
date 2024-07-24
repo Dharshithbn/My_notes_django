bac2d703bc16744be2fea36ba0f2d52a21aa78f0
Dharshithbn <dharshithbn178@gmail.com>
2024-07-24 20:05:07 +0530

    added model

--- My_notes/Mynotes/settings.py
+++ My_notes/Mynotes/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django_bootstrap5",
+    "notes",
 ]
 
 MIDDLEWARE = [
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
--- My_notes/notes/models.py
+++ My_notes/notes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Note(models.Model):
+    note_text = models.CharField( max_length=200)
+    date = models.DateTimeField(default= timezone.now )
+
+    def __str__(self) :
+        return self.note_text
+    
